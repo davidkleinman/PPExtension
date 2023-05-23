4aa83f874fab6883d358cbb71a3bb6586a67beb8
David Kleinman <kldavidt@amazon.com>
2023-05-23 16:01:13 +0300

    fix: Removed unecessary DOMContentParsed listener

--- src/results.js
+++ src/results.js
@@ -1,9 +1,7 @@
-document.addEventListener("DOMContentLoaded", () => {
-    chrome.storage.local.get("data").then((result) => {
-        const resultsParagraph = document.createElement("p");
-        const resultsNode = document.createTextNode(result.data);
-        resultsParagraph.appendChild(resultsNode);
-        document.body.appendChild(resultsParagraph);
-        chrome.storage.local.remove("data");
-    });
+chrome.storage.local.get("data").then((result) => {
+    const resultsParagraph = document.createElement("p");
+    const resultsNode = document.createTextNode(result.data);
+    resultsParagraph.appendChild(resultsNode);
+    document.body.appendChild(resultsParagraph);
+    chrome.storage.local.remove("data");
 });
