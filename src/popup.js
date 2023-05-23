99610850d0d3256fa25af6eadd941c5b9bbbe263
David Kleinman <kldavidt@amazon.com>
2023-05-23 15:55:35 +0300

    feat: Created results page

--- src/popup.js
+++ src/popup.js
@@ -1,17 +1,21 @@
 async function summarize(url) {
     return fetch(`http://localhost:9000/summarize?url=${url}`, {method: "GET"})
         .then((response) => {return response.json()})
-        .then((data) => alert(data["data"]))
+        .then((data) => {return data["data"]})
         .catch((e) => {alert(e)})
 }
 
 document.addEventListener("DOMContentLoaded", () => {
     const summarizeButton = document.getElementById("summarizeButton");
-    summarizeButton.addEventListener("click", () => {
+    summarizeButton.onclick = () => {
         summarizeButton.innerText = "Loading...";
         chrome.tabs.query({ active: true, currentWindow: true }, async (tabs) => {
-            await summarize(tabs[0].url);
-            summarizeButton.innerText = "Summarize Policy";
+            const summarizedPolicy = await summarize(tabs[0].url);
+            chrome.storage.local.set({ "data": summarizedPolicy });
+            summarizeButton.innerText = "View";
+            summarizeButton.onclick = () => {
+                chrome.tabs.create({ url: "src/results.html" });
+            }
         });
-    });
+    };
 });
---
+++ src/results.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    chrome.storage.local.get("data").then((result) => {
+        const resultsParagraph = document.createElement("p");
+        const resultsNode = document.createTextNode(result.data);
+        resultsParagraph.appendChild(resultsNode);
+        document.body.appendChild(resultsParagraph);
+        chrome.storage.local.remove("data");
+    });
+});
