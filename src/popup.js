31ff897f4450ee6e6ecb4dc0912d622b0a9aefd7
David Kleinman <kldavidt@amazon.com>
2023-08-13 12:00:43 +0300

    feat: UI for credentials + validations

--- src/popup.js
+++ src/popup.js
@@ -1,23 +1,46 @@
-async function summarize(url) {
-    return fetch(`http://localhost:9000/summarize?url=${url}`, {method: "GET"})
-        .then((response) => {return response.json()})
-        .then((data) => {return data["data"]});
+const GREEN = "#0ab264";
+const BLUE = "#0095ff";
+const API_URL = "http://localhost:9000/summarize";
+
+async function summarize(url, apiKey, orgId) {
+    return fetch(`${API_URL}?url=${url}&apiKey=${apiKey}&orgId=${orgId}`, {method: "GET"})
+        .then((response) => {
+            if (response.ok) {
+                return response.json();
+            } else if (response.status === 400) {
+                return null;
+            }
+        })
+        .then((data) => {
+            return data ? data["data"] : null;
+        });
+}
+
+async function handleSummarizedPolicy(summarizedPolicy) {
+    if (summarizedPolicy !== null) {
+        chrome.storage.local.set({ "data": summarizedPolicy });
+        summarizeButton.innerText = "View";
+        summarizeButton.style.backgroundColor = GREEN;
+        summarizeButton.onclick = () => {
+            summarizeButton.innerText = "Summarize Policy";
+            summarizeButton.style.backgroundColor = BLUE;
+            chrome.tabs.create({ url: "src/results.html" });
+        }
+    } else {
+        summarizeButton.innerText = "Summarize Policy";
+        alert("The webpage does not contain a privacy policy");
+    }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
     const summarizeButton = document.getElementById("summarizeButton");
     summarizeButton.onclick = () => {
+        const apiKey = document.getElementById("apiKey").value;
+        const orgId = document.getElementById("organizationId").value;
         summarizeButton.innerText = "Loading...";
         chrome.tabs.query({ active: true, currentWindow: true }, async (tabs) => {
-            summarize(tabs[0].url)
-                .then((summarizedPolicy) => {
-                    chrome.storage.local.set({ "data": summarizedPolicy });
-                    summarizeButton.innerText = "View";
-                    summarizeButton.onclick = () => {
-                        summarizeButton.innerText = "Summarize Policy";
-                        chrome.tabs.create({ url: "src/results.html" });
-                    }
-                })
+            summarize(tabs[0].url, apiKey, orgId)
+                .then((summarizedPolicy) => handleSummarizedPolicy(summarizedPolicy))
                 .catch((error) => {
                     summarizeButton.innerText = "Summarize Policy";
                     alert(`Failed to summarize the given privacy policy - ${error}`);
