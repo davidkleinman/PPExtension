603d5ac5bebf1a1e53d03d54bd0f1b97e0493a43
David Kleinman <kldavidt@amazon.com>
2023-09-12 23:02:36 +0300

    fix: major fixes

--- src/popup.js
+++ src/popup.js
@@ -11,35 +11,14 @@ async function summarize(url, apiKey, organizationId) {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify(requestBody),
-    })
-    .then((response) => {
-        if (response.ok) {
-            return response.json();
-        } else if (response.status === 404) {
-            return null;
-        } else {
-            throw new Error("Failed to summarize the given privacy policy")
-        }
-    })
-    .then((data) => {
-        return data ? data["data"] : null;
     });
 }
 
-async function handleSummarizedPolicy(summarizedPolicy, loadingInterval) {
-    clearInterval(loadingInterval);
-    if (summarizedPolicy !== null) {
-        chrome.storage.local.set({ "data": summarizedPolicy });
-        summarizeButton.innerText = "View";
-        summarizeButton.style.backgroundColor = GREEN;
-        summarizeButton.onclick = () => {
-            summarizeButton.innerText = "Summarize Policy";
-            summarizeButton.style.backgroundColor = BLUE;
-            chrome.tabs.create({ url: "src/results.html" });
-        }
+const intervalFunction = () => {
+    if (summarizeButton.innerText === "Loading...") {
+        summarizeButton.innerText = "Loading.";
     } else {
-        summarizeButton.innerText = "Summarize Policy";
-        alert("The webpage does not contain a privacy policy");
+        summarizeButton.innerText += ".";
     }
 }
 
@@ -49,26 +28,28 @@ document.addEventListener("DOMContentLoaded", () => {
         summarizeButton.disabled = true;
         const apiKey = document.getElementById("apiKey").value;
         const organizationId = document.getElementById("organizationId").value;
-        summarizeButton.innerText = "Loading...";
+        summarizeButton.innerText = "Loading.";
+        const loadingInterval = setInterval(intervalFunction, 1000);
         chrome.tabs.query({ active: true, currentWindow: true }, async (tabs) => {
-            const loadingInterval = setInterval(() => {
-                if (summarizeButton.innerText === "Loading...") {
-                    summarizeButton.innerText = "Loading.";
+            summarize(tabs[0].url, apiKey, organizationId).then((response) => {
+                summarizeButton.disabled = false;
+                clearInterval(loadingInterval);
+                if (response.ok) {
+                    response.json().then((json) => {
+                        chrome.storage.local.set({ "data": json["data"] });
+                        summarizeButton.innerText = "View";
+                        summarizeButton.style.backgroundColor = GREEN;
+                        summarizeButton.onclick = () => {
+                            summarizeButton.innerText = "Summarize Policy";
+                            summarizeButton.style.backgroundColor = BLUE;
+                            chrome.tabs.create({ url: "src/results.html" });
+                        }
+                    })
                 } else {
-                    summarizeButton.innerText += ".";
-                }
-            }, 1000);
-            summarize(tabs[0].url, apiKey, organizationId)
-                .then((summarizedPolicy) => {
-                    summarizeButton.disabled = false;
-                    handleSummarizedPolicy(summarizedPolicy, loadingInterval)
-                })
-                .catch((error) => {
-                    summarizeButton.disabled = false;
-                    clearInterval(loadingInterval);
                     summarizeButton.innerText = "Summarize Policy";
-                    alert(error);
-                });
+                    response.text().then(error => alert(error));
+                }
+            })
         });
     };
 });
